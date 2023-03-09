d40e3a6bcc2de0299133571684bf6f2223dbe66e
missymaloney <fdmaloney@mac.com>
2023-03-09 11:16:42 -0500

    changed default directory

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Router basename="/">
           <NavBar />
           <Routes>
-            <Route path="/" exact element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             {/*  <Route path='/:projectId' exact component={Project} /> */}
           </Routes>
